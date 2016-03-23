e0544cdc6371afea8d8932dba98b130eb2a70682
wangfei <929728742@qq.com>
2016-03-23 15:13:01 +0800

    构建项目

---
+++ app/src/main/java/com/retrofit/wangfei/flux_retrofit_rxjava/MainActivity.java
@@ -0,0 +1,20 @@
+package com.retrofit.wangfei.flux_retrofit_rxjava;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Created by Android Studio
+ * User: wangfei
+ * Date: 2016-03-23
+ * Time: 9:57
+ * Description:
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
