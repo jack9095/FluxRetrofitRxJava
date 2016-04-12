6fa89d483cbcdb753d700f2d615ff9e9bf504bf0
wangfeigit <929728742@qq.com>
2016-04-12 11:51:57 +0800

    添加persenter层

---
+++ app/src/main/java/com/retrofit/wangfei/flux_retrofit_rxjava/persenter/LoginPersenter.java
@@ -0,0 +1,13 @@
+package com.retrofit.wangfei.flux_retrofit_rxjava.persenter;
+
+/**
+ * Created by Android Studio
+ * User: wangfei
+ * Date: 2016-04-12
+ * Time: 9:57
+ * QQ: 929728742
+ * Description:
+ */
+public class LoginPersenter {
+
+}
